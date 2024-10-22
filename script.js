e7ed94b03bfc5a086ee165e42cfcab18498b2d93
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-22 15:12:28 +0200

    create class

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log('linked');
+
+
+class Veicolo {
+
+    marca;
+    anno;
+    colore;
+    porte;
+    carburante;
+
+    construvtor(marca, anno, colore, porte, carburante) {
+
+        this.marca = marca
+        this.anno = anno
+        this.colore = colore
+        this.porte = porte
+        this.carburante = carburante
+    }
+}
